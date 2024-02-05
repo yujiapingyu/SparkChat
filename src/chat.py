024ba01d60fc0e25565387f1bedde1dec200ec8b
俞加平 <2507490921@qq.com>
2024-02-05 20:12:06 +0800

    update

--- src/chat.py
+++ src/chat.py
@@ -10,32 +10,7 @@ domain = "generalv2"    # v2.0版本
 Spark_url = "ws://spark-api.xf-yun.com/v3.5/chat"  # v1.5环境的地址
 
 client = SparkApiClient(appid, api_key, api_secret, Spark_url, domain)
-
-
-text =[]
-
-def getText(role,content):
-    print('get text: {}'.format(content))
-    jsoncon = {}
-    jsoncon["role"] = role
-    jsoncon["content"] = content
-    text.append(jsoncon)
-    return text
-
-def getlength(text):
-    length = 0
-    for content in text:
-        temp = content["content"]
-        leng = len(temp)
-        length += leng
-    return length
-
-def checklen(text):
-    while (getlength(text) > 8000):
-        del text[0]
-    return text
-    
-
+ 
 @app.route('/chat', methods=['POST'])
 def chat():
     data = request.json
@@ -48,16 +23,5 @@ def chat():
     return jsonify({"response": ans})
 
 if __name__ == '__main__':
-    # text.clear()
-    # client = SparkApiClient(appid, api_key, api_secret, Spark_url, domain)
-    # while(1):
-    #     inputText = input("\n" +"我:")
-    #     question = checklen(getText("user", inputText)) 
-    #     print('question:',question)   
-    #     print("星火:",end = "")
-    #     ans = client.question(question)
-    #     getText("assistant", ans)
-    #     # print(str(text))
-
     app.run(debug=True)
 
